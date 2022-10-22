b82886d3702690941db6c202a5c933ee1e6a2a10
Alex <alexvkleschov@gmail.com>
2022-10-22 13:19:35 +0400

    add html email sending

--- sender/sender.py
+++ sender/sender.py
@@ -1,5 +1,7 @@
+import os
 import smtplib
 from email.message import EmailMessage
+from jinja2 import Environment, FileSystemLoader
 
 
 sender_email = 'from@email.com'
@@ -16,8 +18,12 @@ with open('test_template.html') as f:
     message.set_content(f.read())
 
 
+env = Environment(loader=FileSystemLoader(f'{os.path.dirname(__file__)}'))  # Указываем расположение шаблонов
+template = env.get_template('test_template.html')  # Загружаем нужный шаблон в переменную
+
+output = template.render()
+
 # Отправляем письмо
+message.add_alternative(output, subtype='html') 
 server.send_message(message)
-
 server.close()  # Закрываете соединение с smtp-сервером
-
