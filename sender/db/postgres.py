d60185bd4713fecdf0389507938c4a4f7848adbf
Alex <alexvkleschov@gmail.com>
2022-10-22 18:28:33 +0400

    add postgres service for using database

---
+++ sender/db/postgres.py
@@ -0,0 +1,50 @@
+import uuid
+
+from psycopg2.extensions import connection
+import psycopg2.extras
+
+from models import Notification
+
+
+class PostgresService:
+    """Управляет взаимодействием с БД POstgreSQL"""
+    def __init__(self, pg_conn: connection, tablename: str):
+        self.pg_conn = pg_conn
+        self.tablename = tablename
+        psycopg2.extras.register_uuid()  # Позволяет сервису принимать uuid в качестве параметров
+
+    def execute(self, query: str, values: tuple = tuple()):
+        """Выполняет один SQL запрос"""
+        curs = self.pg_conn.cursor()
+        curs.execute(query, values)
+        curs.close()
+
+
+    def save_notification_to_db(self, notification: Notification) -> None:
+        """Сохраняет уведомление в БД"""
+        query = f'''INSERT INTO {self.tablename} (notification_id, user_id, content_id, type)
+                    VALUES (%s, %s, %s, %s);'''
+        values = (
+                notification.notification_id,
+                notification.user_id,
+                notification.content_id,
+                notification.type,
+                )
+        self.execute(query, values)
+
+    def get_notifications(self):
+        """Возвращает все уведомления из БД"""
+        query = f"SELECT * FROM notifications;"
+        curs = self.pg_conn.cursor()
+        curs.execute(query, tuple())
+        return curs.fetchall()
+
+    def get_notification_by_id(self, notification_id: uuid.UUID):
+        """Возвращает уведомление по notification_id"""
+        query = f"""SELECT * FROM notifications
+                  WHERE notification_id='{notification_id}';"""
+
+        curs = self.pg_conn.cursor()
+        curs.execute(query)
+        result = curs.fetchall()
+        return result[0] if result else None
--- sender/sender.py
+++ sender/sender.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from email.message import EmailMessage
 
 from psycopg2.extras import DictCursor
@@ -7,18 +8,33 @@ from jinja2 import Environment, FileSystemLoader
 from config import email_server_settings, postgres_settings
 from servers import get_smtp_server_connection
 from db.connections import create_pg_conn
+from db.postgres import PostgresService
+from models import Notification
 
 
-pg_conn = create_pg_conn(
+if __name__ == '__main__':
+    with create_pg_conn(
         dbname=postgres_settings.db_name,
         host=postgres_settings.host,
         port=postgres_settings.port,
         user=postgres_settings.user,
         password=postgres_settings.password,
         cursor_factory=DictCursor
-        )
+        ) as pg_conn:
+            random_notification = Notification(
+                    notification_id=uuid.uuid4(),
+                    user_id=uuid.uuid4(),
+                    content_id="tt3245235",
+                    type='email',
+                    )
 
+            postgres_service = PostgresService(pg_conn=pg_conn, tablename='notifications')
 
+            postgres_service.save_notification_to_db(random_notification)
+            print(postgres_service.get_notifications())
+
+            print(postgres_service.get_notification_by_id(uuid.UUID('8bedeb2e-bee4-40e3-8cbd-647b1e1a2149')))
+'''
 recipient_email = 'durden191@yandex.ru'
 
 # Подключаемся к серверу
@@ -48,3 +64,4 @@ output = template.render()
 message.add_alternative(output, subtype='html') 
 server.send_message(message)
 server.close()  # Закрываете соединение с smtp-сервером
+'''
