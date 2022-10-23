6813816fbd3085e8e9de38b45215962e42567254
IAmIrina <irina.vl.kiseleva@gmail.com>
2022-10-23 18:35:56 +0000

    Add subject to json. Backoff for publisher.

--- render_engine/src/core/publisher.py
+++ render_engine/src/core/publisher.py
@@ -4,23 +4,34 @@ import logging
 import pika
 import pika.exceptions
 from config.settings import RabbitMQSettings
-from utils.backoff import backoff
+from utils.backoff import backoff, backoff_reconnect
 
 logger = logging.getLogger(__name__)
 
 
 class RabbitPublisher():
+
+    def reconnect(self):
+        try:
+            self.connection.close()
+        except BaseException:
+            pass
+        self.connect()
+
     @backoff()
+    def connect(self):
+        self.connection = pika.BlockingConnection(self.parameters)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.params.queue, durable=True, exclusive=False, auto_delete=False)
+        self.channel.confirm_delivery()
+
     def __init__(self, rabbit_params: RabbitMQSettings):
         self.params = rabbit_params
         credentials = pika.PlainCredentials(rabbit_params.username, rabbit_params.password)
-        parameters = pika.ConnectionParameters(rabbit_params.host, rabbit_params.port, credentials=credentials)
-        connection = pika.BlockingConnection(parameters)
-        self.channel = connection.channel()
-        self.channel.queue_declare(queue=rabbit_params.queue, durable=True, exclusive=False, auto_delete=False)
-        self.channel.confirm_delivery()
+        self.parameters = pika.ConnectionParameters(rabbit_params.host, rabbit_params.port, credentials=credentials)
+        self.connect()
 
-    @backoff()
+    @backoff_reconnect()
     def publish(self, message, headers):
         try:
             self.channel.basic_publish(exchange=self.params.exchange,
